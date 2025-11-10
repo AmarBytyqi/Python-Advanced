223943ba465a6e7b7a02aaad52c8ef0850144b8f
Student <amar.bytyci@gmail.com>
2025-11-10 17:48:45 +0100

    commit 67

---
+++ module_18/main.py
@@ -0,0 +1,12 @@
+
+import streamlit as st
+import pandas as pd
+
+st.header('Display Data frame')
+
+data = pd.DataFrame({
+    'Name':['Blerdon','Amar','Obama'],
+    'Age': [15,14,69],
+    'City':['Drenas','Prishtina','Washington']
+})
+st.dataframe(data)
